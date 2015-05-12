52890a433d461b35b2ff113e7dcbd092e0529b5d
gjwang <guijiewan@gmail.com>
2015-05-13 00:29:47 +0800

    add lecture8 files, but image not working

---
+++ cs193p/lecture8/Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by gjwang on 4/23/15.
+//  Copyright (c) 2015 gjwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var loggedInUser: User? { didSet { updateUI() } }
+    var secure: Bool = false { didSet { updateUI() } }
+    
+    private func updateUI(){
+        println("updateUI")
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secure Password" : "Password"
+        nameLabel.text = loggedInUser?.name
+        companyLabel.text = loggedInUser?.company
+        imageView.image = loggedInUser?.image
+    }
+    
+    @IBAction func login() {
+        println("username=\(loginField.text), psw=\(passwordField.text)")
+        
+        loggedInUser = User.login(loginField.text ?? "", password: passwordField.text ?? "")
+    }
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
+    
+    var image: UIImage?{
+        get{
+            return imageView.image
+        }
+        
+        set{
+            imageView.image =  newValue
+            if let constrainedView = imageView {
+                if let newImage = newValue{
+                    aspectRatioConstraint = NSLayoutConstraint(
+                        item: constrainedView,
+                        attribute: .Width,
+                        relatedBy: .Equal,
+                        toItem: constrainedView,
+                        attribute: .Height,
+                        multiplier: newImage.aspectRatio,
+                        constant: 0)
+                }
+            }else{
+                aspectRatioConstraint = nil
+            }
+        }
+    }
+    
+    var aspectRatioConstraint: NSLayoutConstraint? {
+        willSet{
+            if let existingConstraint = aspectRatioConstraint {
+                view.removeConstraint(existingConstraint)
+            }
+        }
+        
+        didSet {
+            if let newConstraint = aspectRatioConstraint {
+                view.addConstraint(newConstraint)
+            }
+        }
+    }
+    
+}
+
+
+extension User{
+    var image: UIImage?{
+        if let image = UIImage(named: login){
+            return image
+        }else{
+            return UIImage(named: "unknown_user")
+        }
+    }
+}
+
+extension UIImage{
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width / size.height : 0
+    }
+}
