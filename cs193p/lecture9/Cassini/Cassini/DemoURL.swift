685db6d868349ab9d5ff5ba0d63cf7800167f2f1
gjwang <guijiewan@gmail.com>
2015-05-17 01:24:05 +0800

    add DemoURL.swift

---
+++ cs193p/lecture9/Cassini/Cassini/DemoURL.swift
@@ -0,0 +1,24 @@
+//
+//  DemoURL.swift
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Stanford = NSURL(string: "http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+    static let Gaoyuanyuan = NSURL(string: "http://h.hiphotos.baidu.com/image/pic/item/94cad1c8a786c91740161e43ca3d70cf3bc757b6.jpg")
+    
+    struct NASA {
+        static let Cassini = NSURL(string: "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")
+//        static let Earth = NSURL(string: "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg")
+//        static let Saturn = NSURL(string: "http://www.nasa.gov/sites/default/files/saturn_collage.jpg")
+        
+        //static let Cassini = NSURL(string: "http://d.hiphotos.baidu.com/image/pic/item/dcc451da81cb39dbade0ef51d2160924ab183090.jpg")
+        static let Earth = NSURL(string: "http://d.hiphotos.baidu.com/image/pic/item/86d6277f9e2f07083310e41feb24b899a901f29c.jpg")
+        static let Saturn = NSURL(string: "http://g.hiphotos.baidu.com/image/pic/item/7c1ed21b0ef41bd5536508f353da81cb39db3d87.jpg")
+        
+    }
+}
